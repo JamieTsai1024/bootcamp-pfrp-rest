76353a3d7a9612be4e4400f6dc7dc7192bd631fe
Ryan Sun <ryiijcsun@gmail.com>
2024-01-17 20:14:25 -0500

    Create Reviewer GET endpoints

--- backend/app/routes/reviewer_routes.py
+++ backend/app/routes/reviewer_routes.py
@@ -9,6 +9,27 @@ from ..services import reviewer_service
 # defines a shared URL prefix for all routes
 blueprint = Blueprint('reviewer', __name__, url_prefix='/reviewer')
 
+# defines GET endpoint for retrieving all reviewers
+@blueprint.route('/', methods=['GET'], strict_slashes=False)
+def get_reviewers():
+    # restaurant_service does the actually fetching of reviewers
+    result = reviewer_service.get_reviewers()
+    # HTTP status code 200 means OK
+    return jsonify(result), 200
+
+
+# defines GET endpoint for retrieving a single reviewer based on a provided id
+@blueprint.route('/<int:id>', methods=['GET'], strict_slashes=False)
+def get_reviewer(id):
+    result = reviewer_service.get_reviewer(id)
+    if result is None:
+        error = {'error': 'reviewer not found'}
+        # HTTP status code 404 means Not Found
+        return jsonify(error), 404
+
+    # HTTP status code 200 means OK
+    return jsonify(result), 200
+
 # defines DELETE endpoint for deleting the restaurant with the provided id
 @blueprint.route('/<int:id>', methods=['DELETE'], strict_slashes=False)
 def delete_reviewer(id):
--- backend/app/services/reviewer_service.py
+++ backend/app/services/reviewer_service.py
@@ -6,6 +6,19 @@ while our business logic is really simple so far, it is beneficial to keep it ap
 separation of concerns leads to maintainable code as the application grows, and also makes the code easier to unit test
 '''
 
+def get_reviewers():
+    # Reviewer is a SQLAlchemy model, we can use convenient methods provided
+    # by SQLAlchemy like query.all() to query the data
+    return [result.to_dict() for result in Reviewer.query.all()]
+
+
+def get_reviewer(id):
+    # get queries by the primary key, which is id for the Reviewer
+    reviewer = Reviewer.query.get(id)
+    if reviewer is None:
+        return reviewer
+    return reviewer.to_dict()
+
 def delete_reviewer(id):
     deleted = Reviewer.query.filter_by(id=id).delete()
     # TODO: Delete reviews
